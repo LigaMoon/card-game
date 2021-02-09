53863f277e2e6e160ec7d7d74faffb20ffc636a5
liga <liga.baikova@gmail.com>
2021-02-09 10:20:07 +0100

    feat: On load, assign 2 random card values

--- assets/js/card-game.js
+++ assets/js/card-game.js
@@ -1,4 +1,5 @@
-data = [
+// Data used for cards
+const data = [
     '1',
     '2',
     '3',
@@ -9,5 +10,40 @@ data = [
     '8',
     '9',
     '10'
-]
+];
 
+// Define global, universal variables
+let usedCards = data;
+const keepCards = [];
+const discardCards = [];
+
+// Define card 1 associated variables
+const cardOne = document.querySelector('#card-one')
+const cardOneBtn = document.querySelector('#card-one-btn')
+
+// Define card 2 associated variables
+const cardTwo = document.querySelector('#card-two')
+const cardTwoBtn = document.querySelector('#card-two-btn')
+
+
+// Removes specific value from an array
+const removeCard = (arr, value) => { 
+    return arr.filter( (e) => { 
+        return e != value; 
+    });
+}
+
+// Gets a random value from usedCards and assigns it to card 1, then removes it from the usedCards array.
+// Repeats the same for card 2
+const randomCards = () => {
+    let cardOneContent = usedCards[Math.floor(Math.random() * usedCards.length)];
+    cardOne.innerHTML = cardOneContent;
+    usedCards = removeCard(usedCards, cardOneContent);
+    
+    let cardTwoContent = usedCards[Math.floor(Math.random() * usedCards.length)];
+    cardTwo.innerHTML = cardTwoContent;
+    usedCards = removeCard(usedCards, cardTwoContent);
+}
+
+// Calls the randomCards function on page load
+randomCards();
