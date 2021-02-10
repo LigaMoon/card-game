ea1e05912724be9fb1f751d56ec9181ad4c0b6af
liga <liga.baikova@gmail.com>
2021-02-10 21:42:27 +0100

    feat: Add data for personalities game

---
+++ assets/js/personalities.js
@@ -0,0 +1,569 @@
+const data = [
+    {
+        row: 1,
+        items: [
+            {
+                name: 'self-confident',
+                color: 'red'
+            },
+            {
+                name: 'structured',
+                color: 'blue'
+            },
+            {
+                name: 'self-sensitive',
+                color: 'green'
+            },
+            {
+                name: 'trusting',
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        row: 2,
+        items: [
+            {
+                name: 'spontaneous',
+                color: 'red'
+            },
+            {
+                name: 'checks with others',
+                color: 'yellow'
+            },
+            {
+                name: 'dreamer',
+                color: 'green'
+            },
+            {
+                name: 'analytical',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 3,
+        items: [
+            {
+                name: 'likes involvment',
+                color: 'yellow'
+            },
+            {
+                name: 'likes organization',
+                color: 'blue'
+            },
+            {
+                name: 'likes being straightforward',
+                color: 'red'
+            },
+            {
+                name: 'likes to explore',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 4,
+        items: [
+            {
+                name: 'stubborn',
+                color: 'blue'
+            },
+            {
+                name: 'dictatorial',
+                color: 'red'
+            },
+            {
+                name: 'rebellious',
+                color: 'yellow'
+            },
+            {
+                name: 'easily offended',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 5,
+        items: [
+            {
+                name: 'demanding',
+                color: 'red'
+            },
+            {
+                name: 'nurturing',
+                color: 'yellow'
+            },
+            {
+                name: 'persistent',
+                color: 'blue'
+            },
+            {
+                name: 'quiet',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 6,
+        items: [
+            {
+                name: 'joiner',
+                color: 'yellow'
+            },
+            {
+                name: 'likes to brainstorm',
+                color: 'green'
+            },
+            {
+                name: 'resists change',
+                color: 'blue'
+            },
+            {
+                name: 'takes charge',
+                color: 'red'
+            }
+        ]
+    },
+    {
+        row: 7,
+        items: [
+            {
+                name: 'cautious',
+                color: 'blue'
+            },
+            {
+                name: 'overgenerous',
+                color: 'green'
+            },
+            {
+                name: 'harmonious',
+                color: 'yellow'
+            },
+            {
+                name: 'energetic',
+                color: 'red'
+            }
+        ]
+    },
+    {
+        row: 8,
+        items: [
+            {
+                name: 'caring/helpful',
+                color: 'yellow'
+            },
+            {
+                name: 'outspoken',
+                color: 'red'
+            },
+            {
+                name: 'steadfast behaviour',
+                color: 'blue'
+            },
+            {
+                name: 'mild mannered',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 9,
+        items: [
+            {
+                name: 'believable',
+                color: 'yellow'
+            },
+            {
+                name: 'forceful',
+                color: 'red'
+            },
+            {
+                name: 'disciplined',
+                color: 'green'
+            },
+            {
+                name: 'possessive',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 10,
+        items: [
+            {
+                name: 'daring',
+                color: 'red'
+            },
+            {
+                name: 'idealist',
+                color: 'green'
+            },
+            {
+                name: 'dutiful',
+                color: 'yellow'
+            },
+            {
+                name: 'playful',
+                color: 'red'
+            }
+        ]
+    },
+    {
+        row: 11,
+        items: [
+            {
+                name: 'logical',
+                color: 'blue'
+            },
+            {
+                name: 'contented',
+                color: 'green'
+            },
+            {
+                name: 'friendly',
+                color: 'yellow'
+            },
+            {
+                name: 'bold/audacious',
+                color: 'red'
+            }
+        ]
+    },
+    {
+        row: 12,
+        items: [
+            {
+                name: '"eager beaver"',
+                color: 'red'
+            },
+            {
+                name: 'imaginative',
+                color: 'green'
+            },
+            {
+                name: 'accurate/precise',
+                color: 'blue'
+            },
+            {
+                name: 'well liked',
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        row: 13,
+        items: [
+            {
+                name: 'reserved',
+                color: 'blue'
+            },
+            {
+                name: 'inventive',
+                color: 'green'
+            },
+            {
+                name: 'charisatic',
+                color: 'red'
+            },
+            {
+                name: 'optimistic',
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        row: 14,
+        items: [
+            {
+                name: 'authoritative',
+                color: 'red'
+            },
+            {
+                name: 'team worker',
+                color: 'yellow'
+            },
+            {
+                name: 'independent',
+                color: 'blue'
+            },
+            {
+                name: 'traditional',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 15,
+        items: [
+            {
+                name: 'talkative',
+                color: 'yellow'
+            },
+            {
+                name: 'restless',
+                color: 'red'
+            },
+            {
+                name: 'conscientious',
+                color: 'blue'
+            },
+            {
+                name: 'modest/unassuming',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 16,
+        items: [
+            {
+                name: 'leader',
+                color: 'red'
+            },
+            {
+                name: 'counsellor',
+                color: 'yellow'
+            },
+            {
+                name: 'designer',
+                color: 'green'
+            },
+            {
+                name: 'controller',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 17,
+        items: [
+            {
+                name: 'meticulous',
+                color: 'blue'
+            },
+            {
+                name: 'workaholic',
+                color: 'red'
+            },
+            {
+                name: 'supportive',
+                color: 'yellow'
+            },
+            {
+                name: 'self-directed',
+                color: 'green'
+            }
+        ]
+    },
+    {
+        row: 18,
+        items: [
+            {
+                name: 'industrious',
+                color: 'red'
+            },
+            {
+                name: 'attentive to details',
+                color: 'blue'
+            },
+            {
+                name: 'metal imager',
+                color: 'green'
+            },
+            {
+                name: 'positive thinker',
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        row: 19,
+        items: [
+            {
+                name: 'task-oriented',
+                color: 'blue'
+            },
+            {
+                name: 'people-oriented',
+                color: 'yellow'
+            },
+            {
+                name: 'idea-oriented',
+                color: 'green'
+            },
+            {
+                name: 'result-oriented',
+                color: 'red'
+            }
+        ]
+    },
+    {
+        row: 20,
+        items: [
+            {
+                name: 'emotional',
+                color: 'green'
+            },
+            {
+                name: 'flexibile/adaptable',
+                color: 'yellow'
+            },
+            {
+                name: 'likes recognition',
+                color: 'red'
+            },
+            {
+                name: 'particular',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 21,
+        items: [
+            {
+                name: 'irritable',
+                color: 'red'
+            },
+            {
+                name: 'rigid',
+                color: 'blue'
+            },
+            {
+                name: 'easily slighted',
+                color: 'green'
+            },
+            {
+                name: 'easily threatened',
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        row: 22,
+        items: [
+            {
+                name: 'indirect',
+                color: 'yellow'
+            },
+            {
+                name: 'frank/candid',
+                color: 'red'
+            },
+            {
+                name: 'careful',
+                color: 'green'
+            },
+            {
+                name: 'strict',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 23,
+        items: [
+            {
+                name: 'goal-oriented',
+                color: 'red'
+            },
+            {
+                name: 'capable',
+                color: 'green'
+            },
+            {
+                name: 'volunteers',
+                color: 'yellow'
+            },
+            {
+                name: 'schedule-oriented',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 24,
+        items: [
+            {
+                name: 'excel in crisis',
+                color: 'red'
+            },
+            {
+                name: 'likes compliments',
+                color: 'yellow'
+            },
+            {
+                name: 'dry humor',
+                color: 'green'
+            },
+            {
+                name: 'avoids causing attention',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 25,
+        items: [
+            {
+                name: 'likes watching people',
+                color: 'green'
+            },
+            {
+                name: 'strong-willed',
+                color: 'red'
+            },
+            {
+                name: 'enthusiastic',
+                color: 'yellow'
+            },
+            {
+                name: 'sets very high standards',
+                color: 'blue'
+            }
+        ]
+    },
+    {
+        row: 26,
+        items: [
+            {
+                name: 'very self-confident',
+                color: 'red'
+            },
+            {
+                name: 'cautious in friends',
+                color: 'green'
+            },
+            {
+                name: 'thorough',
+                color: 'blue'
+            },
+            {
+                name: 'dresses in trendy ways',
+                color: 'yellow'
+            }
+        ]
+    },
+    {
+        row: 27,
+        items: [
+            {
+                name: 'neat and tidy',
+                color: 'blue'
+            },
+            {
+                name: 'looks good on outside',
+                color: 'yellow'
+            },
+            {
+                name: 'avoids conflicts',
+                color: 'green'
+            },
+            {
+                name: 'usually right',
+                color: 'red'
+            }
+        ]
+    }
+];
